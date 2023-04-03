daefc4ea1465c8a2d5d11eeef61713f1684401ae
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-04-03 21:09:27 +0600

    https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import {EnvironmentI} from "./environment.interface";
 
 export const environment: EnvironmentI = {
   mode: "prod",
-  apiUrl: 'http://api.biy.kz/'
+  apiUrl: 'https://api.biy.kz/'
 };
