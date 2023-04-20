cbfe7c9c8e29fe3d9e361645fae6062f26983f93
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-04-20 21:38:16 +0600

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { ToastModule } from 'primeng/toast';
 import {MessagesModule} from "primeng/messages";
 import {MessageService} from "primeng/api";
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NotificationModule} from "../common/notification/notification.module";
 import {AuthGuard} from "../common/guard/auth.guard";
+
+registerLocaleData(localeRu, 'ru');
 
 @NgModule({
   declarations: [
