cb7dcda2b108d5880e57312045ef81fae824e6f3
Yaahh <111268404+tlksmn@users.noreply.github.com>
2023-05-10 18:03:30 +0600

    scaffold me module

---
+++ src/app/me/me.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MeComponent } from './me.component';
+
+
+
+@NgModule({
+  declarations: [
+    MeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MeModule { }
